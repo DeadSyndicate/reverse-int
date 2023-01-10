452fd166737a8aec3e1fc548635ca490d8bc8a7a
DeadSyndicate <avalancheofcreativity@gmail.com>
2023-01-10 16:18:35 +0300

    feat: correct some code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 module.exports = function reverse (n) {
-      n = n + "";
-      return n.split("").reverse().join("");
+    let result = 0;
+    while (n) {
+      result = result * 10 + n % 10;
+      n = Math.floor(n / 10);
+    }
+  
+    return result;
 }
